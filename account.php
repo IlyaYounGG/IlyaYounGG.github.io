6e7f8c748c43307cf6f4eeca20d709aa223c5c07
IlyaYounGG <58914348+IlyaYounGG@users.noreply.github.com>
2020-11-18 07:14:18 +0200

    Update account.php

--- account.php
+++ account.php
@@ -1,7 +1,7 @@
 echo '<HTML>
      <HEAD>
      <TITLE>ERROR [INVALID ACCOUNT]</TITLE>
-     <META HTTP-EQUIV="Refresh" CONTENT="0.5; URL=403.html">
+     <META HTTP-EQUIV="Refresh" CONTENT="0.5; URL=http://ilyavlasov.me/dashboard.html">
      </HEAD>
      <BODY>
      403
